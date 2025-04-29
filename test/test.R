d14b6b02638193bcb88cc59b539b8793d2ddfd3d
Akansha Kumar <akanshas.kumar4@gmail.com>
2025-04-29 13:07:53 +1000

    Initial tests on function
    simple checks on loading on old age

--- test/test.R
+++ test/test.R
@@ -1,7 +1,16 @@
 library(readxl)
 #data : https://aga.gov.au/publications/life-tables/australian-life-tables-2020-22
 mortality_data <- read_excel("test/test_data/australian-life-tables-2020-22_0.xlsx")
-head(mortality_data)
-#all columns have proper column names as for premium calculator function
+head(mortality_data)#all columns have proper column names as for premium calculator function
 
+#All tests below should produce output "TRUE"
 
+#check if loading increase increases premium:
+a <- TermLifePremium(mortality_data, 100000, 52, 0.05, 10, 1) #no loading
+a1 <- TermLifePremium(mortality_data, 100000, 52, 0.05, 10, 1.3) #loading
+a1 > a
+#check that loading doesn't apply in case where insured turns 60 at end of term
+b <- TermLifePremium(mortality_data, 100000, 50, 0.05, 10, 1)
+b1 <- TermLifePremium(mortality_data, 100000, 50, 0.05, 10, 1.3)
+b1 == b
+#
