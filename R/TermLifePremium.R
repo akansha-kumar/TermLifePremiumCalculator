ad676f30407863b74324c6f7ec2e2e09c39583cc
Akansha Kumar <akanshas.kumar4@gmail.com>
2025-04-29 13:01:46 +1000

    Implement tests and test data
    Checking if function created works as should with test data

--- R/TermLifePremium.R
+++ R/TermLifePremium.R
@@ -1,21 +1,21 @@
-TermLifePremium <- function(mortality_table, benefit_amount, start_age, interest_rate, policy_term, old_age_loading){
+TermLifePremium <- function(mortality_table, benefit_amount, age, interest_rate, policy_term, old_age_loading){
   #null results outside mortality data age range
-  end_age <- start_age + policy_term
-  if(end_age > max(mortality_data$Age) | start_age < min(mortality_data$Age)){
+  end_age <- age + policy_term
+  if(end_age > max(mortality_data$Age) | age < min(mortality_data$Age)){
     print("Incompatiable Age Range")
   }  else {
     #initialise premium to 0
     net_premium <- 0
     #premium calculation - sum PV of benefit cost in each year
     for(year in 1:policy_term){
-      qx <- as.numeric(mortality_data[mortality_data$Age == start_age,"qx"])#mortality_rate
-      if(start_age>=60){#loading applies when insured >= 60 only
+      qx <- as.numeric(mortality_data[mortality_data$Age == age,"qx"])#mortality_rate
+      if(age>=60){#loading applies when insured >= 60 only (standard loading age)
         qx <- qx * old_age_loading
       }
       discount_factor <- (1+interest_rate)^(-year)
       PV <- benefit_amount*qx*discount_factor #PV of benefit at specific year
       net_premium <- net_premium + PV #accumulating PV over policy term
-      start_age <- start_age + 1 #increment age for next year of policy term
+      age <- age + 1 #increment age for next year of policy term
     }
 
     return(net_premium)
---
+++ test/test.R
@@ -0,0 +1,7 @@
+library(readxl)
+#data : https://aga.gov.au/publications/life-tables/australian-life-tables-2020-22
+mortality_data <- read_excel("test/test_data/australian-life-tables-2020-22_0.xlsx")
+head(mortality_data)
+#all columns have proper column names as for premium calculator function
+
+
